b094a9f51e85baf13683478479a37b43d7974f40
negrochn <negrochn7@gmail.com>
2020-06-21 22:18:10 +0800

    maoyan 组件编码50%

--- components/index/maoyan.vue
+++ components/index/maoyan.vue
@@ -16,13 +16,113 @@
         </li>
       </ul>
     </div>
-    <div class="scenes-body" />
+    <div class="scenes-body">
+      <!-- hor-container 暂未开发 -->
+      <div class="hot-container" />
+      <div class="coming-container">
+        <div>
+          <div class="slider">
+            <div style="left: 0px; margin: 0 12px;" class="slider-content clearfix">
+              <div v-for="film in comingFilmList" :key="film.path" class="slider-item-film">
+                <nuxt-link :to="film.path">
+                  <img :src="film.imgSrc" alt="" class="image film-img">
+                  <div class="film-info">
+                    <p class="film-score">
+                      <b>
+                        <span>{{ film.wishNum }}</span>
+                        人想看
+                      </b>
+                    </p>
+                    <p class="film-name">
+                      {{ film.filmName }}
+                    </p>
+                  </div>
+                </nuxt-link>
+              </div>
+            </div>
+            <div class="btn btn-next">
+              <i class="iconfont icon-btn_right" />
+            </div>
+            <div class="btn btn-pre">
+              <i class="iconfont icon-btn_left" />
+            </div>
+          </div>
+          <div class="slider-pagination" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Maoyan'
+  name: 'Maoyan',
+  data () {
+    return {
+      comingFilmList: [
+        {
+          filmName: '沉香',
+          path: '/maoyan/films/1318812',
+          imgSrc: '//p0.meituan.net/movie/6adbe9c365a3aaadfb65910bc279bf6a3385927.jpg@214w_297h_1e_1c',
+          wishNum: 1773
+        },
+        {
+          filmName: '致命复活',
+          path: '/maoyan/films/1247400',
+          imgSrc: '//p0.meituan.net/movie/f1210412ff52740cfbc3cca8212834bd406835.jpg@214w_297h_1e_1c',
+          wishNum: 632
+        },
+        {
+          filmName: '天下无拐',
+          path: '/maoyan/films/1222268',
+          imgSrc: '//p0.meituan.net/movie/a19913517ec4dd7b1e8c56a199ceaa4280560.jpg@214w_297h_1e_1c',
+          wishNum: 12449
+        },
+        {
+          filmName: '恋上美人鱼',
+          path: '/maoyan/films/665702',
+          imgSrc: '//p1.meituan.net/movie/eb37c5b78ec0c14aaad707700fcc1443390529.jpg@214w_297h_1e_1c',
+          wishNum: 2667
+        },
+        {
+          filmName: '拆弹专家2',
+          path: '/maoyan/films/1218142',
+          imgSrc: '//p1.meituan.net/movie/ccea56ae49249d482be5997aa98e94691344390.jpg@214w_297h_1e_1c',
+          wishNum: 61949
+        },
+        {
+          filmName: '我想静静',
+          path: '/maoyan/films/343130',
+          imgSrc: '//p0.meituan.net/movie/fd0e0e48f13236ed671f3df0e4ba7429200080.jpg@214w_297h_1e_1c',
+          wishNum: 6425
+        },
+        {
+          filmName: '北平会馆',
+          path: '/maoyan/films/1301478',
+          imgSrc: '//p0.meituan.net/movie/644b66958ccbeb8bac4955c52864c8881471793.jpg@214w_297h_1e_1c',
+          wishNum: 24483
+        },
+        {
+          filmName: '通往春天的列车',
+          path: '/maoyan/films/1219866',
+          imgSrc: '//p0.meituan.net/moviemachine/331c70099c5728d53694009426fb84ba165027.jpg@214w_297h_1e_1c',
+          wishNum: 8823
+        },
+        {
+          filmName: '赤狐书生',
+          path: '/maoyan/films/1277751',
+          imgSrc: '//p1.meituan.net/movie/7d4e340665573384faa2f2e34b705b73977180.jpg@214w_297h_1e_1c',
+          wishNum: 23539
+        },
+        {
+          filmName: '哪吒重生',
+          path: '/maoyan/films/1299124',
+          imgSrc: '//p0.meituan.net/movie/d8bba55fde322a809b6eeb95ddbffec82554093.jpg@214w_297h_1e_1c',
+          wishNum: 9720
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -81,6 +181,70 @@ export default {
   }
   .mf-shang-hei-regular {
     font-family: "MFShangHei-Regular" !important;
+  }
+  .scenes-body {
+    .coming-container {
+    }
+  }
+  .slider {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    .slider-content {
+      position: relative;
+      width: 10000000px;
+      height: 100%;
+      transition: left .5s ease-out;
+      .slider-item-film {
+        position: relative;
+        float: left;
+        margin-right: 19px;
+        width: 214px;
+        height: 297px;
+        background-size: 100%;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        .film-img {
+          width: 100%;
+          height: 100%;
+          border-radius: 4px;
+        }
+        .film-info {
+          position: absolute;
+          bottom: 0;
+          width: 100%;
+          height: 100px;
+          border-bottom-left-radius: 4px;
+          border-bottom-right-radius: 4px;
+          background-image: linear-gradient(-180deg, rgba(0,0,0,0.00) 0%, rgba(29,45,55,0.80) 99%);
+          .film-score {
+            margin-top: 48px;
+            padding-left: 10px;
+            font-size: 12px;
+            color: $color-white;
+            font-weight: 500;
+            text-align: left;
+            span {
+              font-size: 16px;
+              color: #FD9827;
+              font-weight: 500;
+            }
+          }
+          .film-name {
+            padding-left: 10px;
+            width: 6em;
+            font-size: 16px;
+            color: $color-white;
+            font-weight: 500;
+            text-align: left;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          }
+        }
+      }
+    }
   }
 }
 </style>
