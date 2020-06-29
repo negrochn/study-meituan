0bd2cd25e57f60a7b7e934e0bacee02b7e164aed
negrochn <negrochn7@gmail.com>
2020-06-29 22:20:41 +0800

    增加 score 组件，并解决 margin 塌陷问题

---
+++ components/index/score.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="score">
+    <div class="rate-stars">
+      <ul class="rate-stars-ul rate-stars-dark">
+        <li><i class="iconfont icon-star_icon" /></li>
+        <li><i class="iconfont icon-star_icon" /></li>
+        <li><i class="iconfont icon-star_icon" /></li>
+        <li><i class="iconfont icon-star_icon" /></li>
+        <li><i class="iconfont icon-star_icon" /></li>
+      </ul>
+      <ul :style="lightWidth" class="rate-stars-ul rate-stars-light">
+        <li><i class="iconfont icon-star_icon" /></li>
+        <li><i class="iconfont icon-star_icon" /></li>
+        <li><i class="iconfont icon-star_icon" /></li>
+        <li><i class="iconfont icon-star_icon" /></li>
+        <li><i class="iconfont icon-star_icon" /></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Score',
+  props: {
+    score: {
+      type: String || Number,
+      default: 0
+    }
+  },
+  computed: {
+    lightWidth () {
+      return `width: ${this.score / 5 * 100}%;`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.score {
+  .rate-stars {
+    position: relative;
+    display: inline-block;
+    width: 84px;
+    height: 24px;
+    .rate-stars-ul {
+      margin: 3px 0;
+      padding: 0;
+      text-align: left;
+      white-space: nowrap;
+      &>li {
+        display: inline-block;
+        padding: 0 2px;
+      }
+    }
+    .rate-stars-dark {
+      color: #c3c3c3;
+    }
+    .rate-stars-light {
+      position: absolute;
+      top: 0;
+      color: #FF9900;
+      overflow: hidden;
+    }
+  }
+}
+</style>
