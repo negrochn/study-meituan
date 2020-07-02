91afa49f939fc15de3cf1e4b31ef23ec9d048844
negrochn <j.chen@hzgosun.com>
2020-07-02 10:06:34 +0800

    提取 minsu-item 组件，并优化代码

---
+++ components/index/minsu-item.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="minsu-item">
+    <nuxt-link to="/">
+      <div class="product-card-header">
+        <img :src="product.coverImage" alt="" class="product-img">
+        <img src="https://hz.meituan.com/null@200w_200h_1e_1c" alt="" class="head-img">
+      </div>
+      <div class="product-info">
+        <p class="product-title">
+          {{ product.title }}
+        </p>
+        <p class="sub-title">
+          <span>整套{{ product.layoutRoom }}居室·</span>
+          <span>可住{{ product.maxGuestNumber }}人 | </span>
+          <span>{{ product.locationArea }}</span>
+        </p>
+        <p class="price-number numfont price">
+          <span class="price-icon">￥</span>
+          <span>{{ product.price }}</span>
+        </p>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MinsuItem',
+  components: {},
+  mixins: [],
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/css/variables.scss';
+
+.minsu-item {
+  float: left;
+  margin-right: 18px;
+  .product-card-header {
+    position: relative;
+    .product-img {
+      margin-bottom: 11px;
+      width: 370px;
+      height: 208px;
+      max-width: 100%;
+      background: url(//p0.meituan.net/travelcube/214b8411190e874781fe91dd3096236211567.png);
+      background-size: cover;
+      background-position: 50%;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    .head-img {
+      position: absolute;
+      bottom: -12px;
+      right: 15px;
+      width: 48px;
+      height: 48px;
+      border-radius: 40px;
+      background-image: url('https://p0.meituan.net/mmc/35ad1f9253761ea6ff822b5e659f234f3758.png');
+      background-size: 100%;
+      background-position: 50% 50%;
+      background-repeat: no-repeat;
+      border: 2px solid $color-white;
+    }
+  }
+  .product-info {
+    padding-bottom: 25px;
+    .product-title {
+      margin: 7px 0;
+      width: 297px;
+      font-size: 16px;
+      color: $color-text-primary;
+      font-weight: 400;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .sub-title {
+      font-size: 12px;
+      color: $color-text-secondary;
+    }
+    .price {
+      padding: 4px 0;
+      font-size: 22px;
+      color: #FF6600;
+      font-weight: 500;
+      letter-spacing: -.8px;
+      .price-icon {
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
--- components/index/zhenguo.vue
+++ components/index/zhenguo.vue
@@ -10,28 +10,7 @@
     />
     <div class="minsu-ls-view clearfix">
       <div v-for="item in minsuList" :key="item.productId" :style="`display: ${curCity === item.city ? 'flex' : 'none'};`" class="products">
-        <div v-for="product in item.productList" :key="product.productId" class="minsu-item">
-          <nuxt-link to="/">
-            <div class="product-card-header">
-              <img :src="product.coverImage" alt="" class="product-img">
-              <img src="https://hz.meituan.com/null@200w_200h_1e_1c" alt="" class="head-img">
-            </div>
-            <div class="product-info">
-              <p class="product-title">
-                {{ product.title }}
-              </p>
-              <p class="sub-title">
-                <span>整套{{ product.layoutRoom }}居室·</span>
-                <span>可住{{ product.maxGuestNumber }}人 | </span>
-                <span>{{ product.locationArea }}</span>
-              </p>
-              <p class="price-number numfont price">
-                <span class="price-icon">￥</span>
-                <span>{{ product.price }}</span>
-              </p>
-            </div>
-          </nuxt-link>
-        </div>
+        <MinsuItem v-for="product in item.productList" :key="product.productId" :product="product" />
       </div>
     </div>
   </div>
@@ -39,11 +18,13 @@
 
 <script>
 import IndexNavContainer from './index-nav-container'
+import MinsuItem from './minsu-item'
 
 export default {
   name: 'Zhenguo',
   components: {
-    IndexNavContainer
+    IndexNavContainer,
+    MinsuItem
   },
   data () {
     return {
@@ -1109,64 +1090,6 @@ export default {
     .products {
       display: flex;
       flex-wrap: wrap;
-    }
-    .minsu-item {
-      float: left;
-      margin-right: 18px;
-      .product-card-header {
-        position: relative;
-        .product-img {
-          margin-bottom: 11px;
-          width: 370px;
-          height: 208px;
-          max-width: 100%;
-          background: url(//p0.meituan.net/travelcube/214b8411190e874781fe91dd3096236211567.png);
-          background-size: cover;
-          background-position: 50%;
-          border-radius: 4px;
-          cursor: pointer;
-        }
-        .head-img {
-          position: absolute;
-          bottom: -12px;
-          right: 15px;
-          width: 48px;
-          height: 48px;
-          border-radius: 40px;
-          background-image: url('https://p0.meituan.net/mmc/35ad1f9253761ea6ff822b5e659f234f3758.png');
-          background-size: 100%;
-          background-position: 50% 50%;
-          background-repeat: no-repeat;
-          border: 2px solid $color-white;
-        }
-      }
-      .product-info {
-        padding-bottom: 25px;
-        .product-title {
-          margin: 7px 0;
-          width: 297px;
-          font-size: 16px;
-          color: $color-text-primary;
-          font-weight: 400;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-        }
-        .sub-title {
-          font-size: 12px;
-          color: $color-text-secondary;
-        }
-        .price {
-          padding: 4px 0;
-          font-size: 22px;
-          color: #FF6600;
-          font-weight: 500;
-          letter-spacing: -.8px;
-          .price-icon {
-            font-size: 14px;
-          }
-        }
-      }
     }
   }
 }
